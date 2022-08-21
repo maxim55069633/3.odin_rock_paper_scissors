73d5dc6d176c0eddf49ec30909ff277d5b73f3f6
maxim55069633 <1246003314@qq.com>
2022-08-21 13:19:22 +0800

    version 2.0 add a simple UI

--- README.md
+++ README.md
@@ -1,23 +1,34 @@
 # odin_rock_paper_scissors
-This project is about the classic game "Rock Paper Scissors" using JavaScript. Players can input their selection among the three options five times. Their input could be case-insensitive. Then the console will claim who is the winner.
 
+version 2.0
+
+Add simple UI. Now users can select the elements by clicking on the images. They don't need to check the outcome via the console. 
+
+Disclaimer: all the images come from me.
+
+
+---
+
+version 1.0
+
+This project is about the classic game "Rock Paper Scissors" using JavaScript. Players can input their selection among the three options five times. Their input could be case-insensitive. Then the console will claim who is the winner.
 
 a mistake I made:
 At first, I wrote 
-```
+"
 function standardizeInput(Selection)
 {
     return Selection.toLowerCase().replace(Selection[0], Selection[0].toUpperCase() );
 }
-```
+"
 but later I found I failed to change some input into the standard(first letter uppercase, the rest letter lowercase). For example Rock ends up like rock. This is because "Rock".toLowerCase() would be "rock". At this point, Selection[0] is still "R" since toLowerCase won't change the original string. It's impossible to find a "R" in the rock so our expectation is not met.
 so I modified this function like:
-```
+"
 function standardizeInput(Selection)
 {
     return Selection.toLowerCase().replace(Selection[0].toLowerCase(), Selection[0].toUpperCase() );
 }
-```
+" 
 
 note:
 1.
--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 let playerScore=0;
 let ComputerScore=0;
+let roundNum=0;
 
 function getComputerChoice() {
     let randomNum=Math.random()
@@ -16,66 +17,100 @@ function getComputerChoice() {
     return ComputerChoice;
 }
 
-function standardizeInput(Selection)
+function reloadMygame()
 {
-    // Capitalize the first letter, and leave all the rest letters lowercase.
-    // be careful in Scissors, I should only substitute the first letter, not the rest s.
-    return Selection.toLowerCase().replace(Selection[0].toLowerCase(), Selection[0].toUpperCase() );
-   // be careful of the Selection[0] in the above. Because selection[0] doesn't be changed by the toLowerCase permanently.
- 
+    window.location.reload();
+}
+
+function playAgain(winner)
+{
+    let change=document.querySelector(".threeoptions");
+    change.innerHTML=`
+        <p>The winner is ${winner}</p> 
+        <button class="play_again" onclick=window.location.reload();> Play Again </button>`;
+    
+    change.classList.add('play_again');
+
+    // let newScript=document.createElement("script");
+    // let inlineScript=document.createTextNode(`
+    // document.querySelector('button').addEventListener('click',reloadMygame)`);
+    // newScript.appendChild(inlineScript);
+    // change.appendChild(newScript);
 }
 
 function playRound(playerSelection, computerSelection) {
-    let std_playerSelection = standardizeInput(playerSelection);
-    let std_computerSelection = standardizeInput(computerSelection);
+
+    roundNum +=1;
     let outcome; 
-    if ( std_playerSelection === std_computerSelection )
-        outcome = "The game is tied in this round."
-    else if ( std_playerSelection === "Scissors" && std_computerSelection === "Paper" )
+    if ( playerSelection === computerSelection )
     {
-        playerScore = playerScore +1;
-        outcome = `You Win in this round! ${std_playerSelection} beat ${std_computerSelection}`;
+
+        outcome = `The game is tied.`;
     }
-    else if ( std_playerSelection === "Rock" && std_computerSelection === "Scissors")
+    else if ( playerSelection === "Scissors" && computerSelection === "Paper" )
     {
         playerScore = playerScore +1;
-        outcome = `You Win in this round! ${std_playerSelection} beat ${std_computerSelection}`;
+        outcome = `You win`;
     }
-    else if ( std_playerSelection === "Paper" && std_computerSelection === "Rock")
+    else if ( playerSelection === "Rock" && computerSelection === "Scissors")
     {
         playerScore = playerScore +1;
-        outcome = `You Win in this round! ${std_playerSelection} beat ${std_computerSelection}`;
+        outcome = `You win`;
+    }
+    else if ( playerSelection === "Paper" && computerSelection === "Rock")
+    {
+        playerScore = playerScore +1;
+        outcome = `You win`;
     }
     else 
     {
         ComputerScore = ComputerScore+1;
-        outcome = `You Lose in this round! ${std_computerSelection} beat ${std_playerSelection}`;
+        outcome = `You lose`;
     }
-    return outcome;
+    let output= outcome+` in Round ${roundNum}. Your selection is ${playerSelection} while the Computer's selection is ${computerSelection}`;
+    
+    document.querySelector('.player_score').textContent=playerScore;
+    document.querySelector('.computer_score').textContent=ComputerScore;
+    if(outcome === `You win`)
+    {
+        document.querySelector('.info').classList.remove('tie');
+        document.querySelector('.info').classList.remove('lose');
+        document.querySelector('.info').classList.add('win');
+    }
+    else if (outcome === `You lose`)
+    {
+
+        document.querySelector('.info').classList.remove('tie');
+        document.querySelector('.info').classList.remove('win');
+        document.querySelector('.info').classList.add('lose');
+    } else {
+        document.querySelector('.info').classList.remove('lose');
+        document.querySelector('.info').classList.remove('win');
+        document.querySelector('.info').classList.add('tie');
+    }
+        
+    document.querySelector('.info').textContent = output;
+
+    if(playerScore === 5 || ComputerScore ===5)
+    {
+        let winner;
+        if(playerScore === 5)
+            winner="Player";
+        else
+            winner="Computer";
+        playAgain(winner);
+    }
   }
 
-function game()
+function submitplayerchoice (e)
 {
-    for (let i=0; i <5; i++)
-    {
-        let playerSelection=prompt(`Please input "Paper", "Rock", or "Scissors"`, "rock");
-        let computerSelection=getComputerChoice();
-        let std_playerSelection=standardizeInput(playerSelection);
-        let std_computerSelection=standardizeInput(computerSelection);
-        console.log(`Round ${i+1}, your selection is ${std_playerSelection} while the computer's selection is ${std_computerSelection}`);
-        console.log(playRound(playerSelection, computerSelection));
-    }
+    const playerSelection=this.getAttribute('class');
+    const computerSelection=getComputerChoice();
+    playRound(playerSelection, computerSelection);
 
-    if (playerScore === ComputerScore)
-    {
-        console.log(`You and the computer are tied up in this game. Your score is ${playerScore} while the computer's score is ${ComputerScore}`);
-    } 
-        else if (playerScore > ComputerScore)
-    {
-        console.log(`You win this game! Your score is ${playerScore} while the computer's score is ${ComputerScore}`);
-    }  else {
-        console.log(`You lose this game! Your score is ${playerScore} while the computer's score is ${ComputerScore}`)
-    }
 }
 
-game();
+const mySelections= document.querySelectorAll('.threeoptions button');
+mySelections.forEach(
+    selection=>selection.addEventListener('click',submitplayerchoice)
+)
